3e1c373512c7e25aec239c47576a6c3c423c1900
Christoph Ungricht <christoph.ungricht@outlook.com>
2023-09-09 18:55:25 +0200

    Add possibility to drag and drop.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- main.c
+++ main.c
@@ -8,17 +8,20 @@
 
 #define FPS 60
 
+char *getFirstFile(FilePathList files) {
+  assert(files.count > 0 && "No files found");
+  return files.paths[0];
+}
+
 int main(void) {
 
-  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT,
-             "raylib [audio] example - music loading and playing");
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "musializer");
   InitAudioDevice();
-  Music music = LoadMusicStream(
-      "Schoolhouse Trouble - REQUIEM.wav"); // Load music from file
-  PlayMusicStream(music);
+
+  // Music that will be played
+  Music music;
 
   float timePlayed = 0.0f; // Time played normalized [0.0f..1.0f]
-  bool pause = false;      // Music playing paused
 
   SetTargetFPS(FPS); // Set our game to run at 30 frames-per-second
   //--------------------------------------------------------------------------------------
@@ -26,47 +29,64 @@ int main(void) {
   // Main game loop
   while (!WindowShouldClose()) // Detect window close button or ESC key
   {
+
     // Update
     //----------------------------------------------------------------------------------
-    UpdateMusicStream(music); // Update music buffer with new stream data
+    if (IsFileDropped()) {
 
-    // Restart music playing (stop and play)
-    if (IsKeyPressed(KEY_SPACE)) {
-      StopMusicStream(music);
+      FilePathList files = LoadDroppedFiles();
+      char *file_path = getFirstFile(files);
+      music = LoadMusicStream(file_path);
       PlayMusicStream(music);
+      UnloadDroppedFiles(files);
     }
 
-    // Pause/Resume music playing
-    if (IsKeyPressed(KEY_P)) {
-      pause = !pause;
+    if (IsMusicReady(music)) {
+      UpdateMusicStream(music); // Update music buffer with new stream data
 
-      if (pause)
-        PauseMusicStream(music);
-      else
-        ResumeMusicStream(music);
+      // Restart music playing (stop and play)
+      if (IsKeyPressed(KEY_SPACE)) {
+        StopMusicStream(music);
+        PlayMusicStream(music);
+      }
+
+      // Pause/Resume music playing
+      if (IsKeyPressed(KEY_P)) {
+
+        if (IsMusicStreamPlaying(music))
+          PauseMusicStream(music);
+        else
+          ResumeMusicStream(music);
+      }
+
+      // Get normalized time played for current music stream
+      timePlayed = GetMusicTimePlayed(music) / GetMusicTimeLength(music);
+
+      if (timePlayed > 1.0f)
+        timePlayed = 1.0f; // Make sure time played is no longer than music
     }
 
-    // Get normalized time played for current music stream
-    timePlayed = GetMusicTimePlayed(music) / GetMusicTimeLength(music);
-
-    if (timePlayed > 1.0f)
-      timePlayed = 1.0f; // Make sure time played is no longer than music
     //----------------------------------------------------------------------------------
 
     // Draw
     //----------------------------------------------------------------------------------
     BeginDrawing();
 
-    ClearBackground(RAYWHITE);
+    ClearBackground(BLACK);
 
-    DrawText("MUSIC SHOULD BE PLAYING!", 255, 150, 20, LIGHTGRAY);
+    if (IsMusicReady(music)) {
 
-    DrawRectangle(200, 200, 400, 12, LIGHTGRAY);
-    DrawRectangle(200, 200, (int)(timePlayed * 400.0f), 12, MAROON);
-    DrawRectangleLines(200, 200, 400, 12, GRAY);
+      DrawText("MUSIC SHOULD BE PLAYING!", 255, 150, 20, WHITE);
 
-    DrawText("PRESS SPACE TO RESTART MUSIC", 215, 250, 20, LIGHTGRAY);
-    DrawText("PRESS P TO PAUSE/RESUME MUSIC", 208, 280, 20, LIGHTGRAY);
+      DrawRectangle(200, 200, 400, 12, WHITE);
+      DrawRectangle(200, 200, (int)(timePlayed * 400.0f), 12, RED);
+      DrawRectangleLines(200, 200, 400, 12, LIGHTGRAY);
+
+      DrawText("PRESS SPACE TO RESTART MUSIC", 215, 250, 20, WHITE);
+      DrawText("PRESS P TO PAUSE/RESUME MUSIC", 208, 280, 20, WHITE);
+    } else {
+      DrawText("DRAG AND DROP A MUSIC FILE", 235, 200, 20, WHITE);
+    }
 
     EndDrawing();
     //----------------------------------------------------------------------------------
