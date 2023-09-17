4684b8254341fab9c7d8c01b4e3a3dcf4a98685e
Christoph Ungricht <christoph.ungricht@outlook.com>
2023-09-17 17:23:28 +0200

    Moved source files to src folder.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ CFLAGS="-Wall -Wextra -Wpedantic -Og $(pkg-config --cflags raylib)"
 LFLAGS="$(pkg-config  --libs raylib) -lpthread -lm"
 
 # shellcheck disable=SC2086
-cc -o ./build/musializer ./main.c $CFLAGS $LFLAGS -ldl 
+cc -o ./build/musializer ./src/main.c $CFLAGS $LFLAGS -ldl 
 
 # Tests
 CFLAGS_TEST="-Wall -Wextra -Wpedantic -Ofast"
 LFLAGS_TEST="-lm"
 
 # shellcheck disable=SC2086
-cc fft.c fft_test.c -o ./build/fft_test $CFLAGS_TEST $LFLAGS_TEST
+cc ./src/fft.c ./src/fft_test.c -o ./build/fft_test $CFLAGS_TEST $LFLAGS_TEST
--- build_plug.sh
+++ build_plug.sh
@@ -8,4 +8,4 @@ CFLAGS="-Wall -Wextra -Wpedantic -Og $(pkg-config --cflags raylib)"
 LFLAGS="$(pkg-config  --libs raylib) -lpthread -lm"
 
 # shellcheck disable=SC2086
-cc -o ./build/libplug.so ./plug.c ./fft.c $CFLAGS $LFLAGS -fPIC -shared
+cc -o ./build/libplug.so ./src/plug.c ./src/fft.c $CFLAGS $LFLAGS -fPIC -shared
