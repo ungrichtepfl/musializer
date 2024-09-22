7b96ac9ce57d7d79ac0ad0b865953c2cb391af48
Christoph Ungricht <ch.ungricht@scewo.ch>
2024-09-22 15:31:51 +0200

    feat: Added raylib5.0.0 binaries.
    Signed-off-by: Christoph Ungricht <ch.ungricht@scewo.ch>

--- build.sh
+++ build.sh
@@ -4,21 +4,26 @@ set -xe
 
 mkdir -p ./build
 
-RAYLIB_VERSION="4.2.0" # Corresponds to raylib 4.5
-
 if [ "$1" = "--dynamic" ] || [ "$1" = "-d" ]; then
+    ./build_plug.sh
     CFLAGS="-Wall -Wextra -Wpedantic -Ofast"
     LFLAGS="-ldl"
     CPPFLAGS="-DDYLIB"
-    RAYLIB=""
+    RAYLIB="" # No raylib needed, will be handled by build_plug.sh
     SRC="./src/main.c"
 else
     CFLAGS="-Wall -Wextra -Wpedantic -Ofast"
     LFLAGS="-lm -lpthread"
     CPPFLAGS=""
-    RAYLIB="$(pkg-config --libs --cflags "raylib = $RAYLIB_VERSION")"
+    if [ "$(uname -m)" = "x86_64" ]; then
+        RAYLIB="-I ./raylib-5.0_linux_amd64/include/ -L./raylib-5.0_linux_amd64/lib -l:libraylib.a -ldl"
+    else
+        echo "You are not on a x86_64 machine, please install raylib 5.0.0 and make sure that pkg-config can find it."
+        RAYLIB="$(pkg-config --libs --cflags "raylib")"
+    fi
     SRC="./src/main.c ./src/plug.c ./src/fft.c"
 fi
+
 
 # shellcheck disable=SC2086
 cc -o ./build/musializer $SRC $CPPFLAGS $CFLAGS $RAYLIB $LFLAGS
--- build_plug.sh
+++ build_plug.sh
@@ -4,10 +4,18 @@ set -xe
 
 mkdir -p ./build
 
-RAYLIB_VERSION="4.2.0" # Corresponds to raylib 4.5
 
-CFLAGS="-Wall -Wextra -Wpedantic -Ofast $(pkg-config --cflags "raylib = $RAYLIB_VERSION")"
-LFLAGS="$(pkg-config  --libs "raylib = $RAYLIB_VERSION") -lpthread -lm"
+if [ "$(uname -m)" = "x86_64" ]; then
+    RAYLIB_CFLAGS="-I ./raylib-5.0_linux_amd64/include/"
+    RAYLIB_LFLAGS="-L./raylib-5.0_linux_amd64/lib -l:libraylib.so -Wl,-rpath=./raylib-5.0_linux_amd64/lib"
+else
+    echo "You are not on a x86_64 machine, please install raylib 5.0.0 and make sure that pkg-config can find it."
+    RAYLIB_LFLAGS="$(pkg-config --libs "raylib")"
+    RAYLIB_CFLAGS="$(pkg-config --flags "raylib")"
+fi
+
+CFLAGS="-Wall -Wextra -Wpedantic -Ofast $RAYLIB_CFLAGS"
+LFLAGS="-lpthread -lm $RAYLIB_LFLAGS"
 
 # shellcheck disable=SC2086
 cc -c -o ./build/plug.o ./src/plug.c $CFLAGS -fPIC
