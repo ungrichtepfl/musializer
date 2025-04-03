8a4222fa9154fac66d4ce4487d5f2e52cab05ffd
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-04-03 15:02:38 -0300

    refactor: Use ES6 modules.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- build_web.sh
+++ build_web.sh
@@ -11,7 +11,7 @@ emcc -o build/musializer.js \
   -Os -Wall \
   -lm -lpthread -ldl \
   -I ./raylib-5.0_wasm/include/ -L./raylib-5.0_wasm/lib -l:libraylib.a \
-  -sUSE_GLFW=3 -sASYNCIFY -sMODULARIZE=1 -sEXPORT_NAME=createMusializer \
+  -sUSE_GLFW=3 -sASYNCIFY -sMODULARIZE=1 -sEXPORT_ES6=1 -sEXPORT_NAME=createMusializer \
   -sTOTAL_STACK=512mb -DPLATFORM_WEB \
 
 cp build/musializer.js build/musializer.wasm .
